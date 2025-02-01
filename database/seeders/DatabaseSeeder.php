bde17da3074e666b9eb7a1e5768173818c11feed
terracyrus <59580089+terracyrus@users.noreply.github.com>
2025-02-01 18:12:12 +0100

    discipline: add new model including factory, controller and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Discipline;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -32,6 +33,18 @@ class DatabaseSeeder extends Seeder
             'name' => 'Operator',
             'email' => 'operator@example.com',
             'role' => 'operator',
+        ]);
+
+        Discipline::factory()->create([
+            'name' => 'Puzzle',
+        ]);
+
+        Discipline::factory()->create([
+            'name' => 'Bibel',
+        ]);
+
+        Discipline::factory()->create([
+            'name' => 'Karten',
         ]);
     }
 }
