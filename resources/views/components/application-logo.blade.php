af3e61fd0e33b9df4069ac8ba3f1c6a4312ce89d
terracyrus <59580089+terracyrus@users.noreply.github.com>
2025-02-11 20:44:13 +0100

    welcome: anpassung Logo grösse und willkommensnachricht

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('BESJ-Logo_trspAnker_Vektoren.svg') }}" alt="BESJ-Logo transparent Anker" class="h-10 w-10" />
+<img src="{{ asset('BESJ-Logo_trspAnker_Vektoren.svg') }}" alt="BESJ-Logo transparent Anker" class="h-12" />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
+    <h1 class="text-center text-2xl font-bold">Willkommen beim Jungschigame vom trhekrev Teamweekend!</h1>
     <a
         href="{{ route('login') }}"
         class="flex content-center justify-center rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
