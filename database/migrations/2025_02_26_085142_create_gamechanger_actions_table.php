94760ba1a2f5533158411f207fc4a0a0b8e58372
terracyrus <59580089+terracyrus@users.noreply.github.com>
2025-02-26 11:08:02 +0100

    gamechangers_action: update constrained

--- database/migrations/2025_02_26_085142_create_gamechanger_actions_table.php
+++ database/migrations/2025_02_26_085142_create_gamechanger_actions_table.php
@@ -10,10 +10,10 @@ return new class extends Migration
     {
         Schema::create('gamechanger_actions', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\Gamechanger::class)->constrained()->onDelete('cascade');
-            $table->foreignId('requested_by')->constrained(\App\Models\User::class)->onDelete('cascade'); // Wer hat ihn angefragt?
-            $table->foreignId('executed_by')->constrained(\App\Models\User::class)->onDelete('set null'); // Wer hat ihn ausgeführt?
-            $table->foreignId('target_user')->nullable()->constrained(\App\Models\User::class)->onDelete('set null'); // Optional: Wer wurde getroffen?
+            $table->foreignIdFor(\App\Models\Gamechanger::class)->constrained()->cascadeOnUpdate()->onDelete('cascade');
+            $table->foreignId('requested_by')->constrained('user_id')->cascadeOnUpdate()->onDelete('cascade'); // Wer hat ihn angefragt?
+            $table->foreignId('executed_by')->constrained('user_id')->cascadeOnUpdate()->onDelete('set null'); // Wer hat ihn ausgeführt?
+            $table->foreignId('target_user')->nullable()->constrained('user_id')->cascadeOnUpdate()->onDelete('set null'); // Optional: Wer wurde getroffen?
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
         });
