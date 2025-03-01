b01dcba300f43ef9f68ba7bb86b49f93ea97c78c
terracyrus <59580089+terracyrus@users.noreply.github.com>
2025-03-01 20:10:45 +0100

    leaderboard: update view

--- resources/views/livewire/leaderboard-table.blade.php
+++ resources/views/livewire/leaderboard-table.blade.php
@@ -1,14 +1,17 @@
 <div wire:poll.5s>
     <!-- Refresh table every 5 seconds -->
     <x-table>
-        <x-table.head :headers="['Platz', 'Gruppe', 'Differenz', 'Punkte']" />
+        <x-table.head :headers="['Platz', 'Gruppe', 'Differenz', 'Ziel / Punkte']" />
         <x-table.body>
             @foreach ($sortedPlayers as $index => $entry)
                 <x-table.row :entry="$entry" :highlight="Auth::id()">
                     <td>{{ $index + 1 }}</td>
                     <td>{{ $entry->user->name }}</td>
                     <td>{{ $entry->difference }}</td>
-                    <td class="text-right">{{ $entry->first_points }} / {{ $entry->overall_points }}</td>
+                    <td class="text-right">
+                        <p class="font-bold">{{ $entry->first_points }}</p>
+                        / {{ $entry->overall_points }}
+                    </td>
                 </x-table.row>
             @endforeach
         </x-table.body>
